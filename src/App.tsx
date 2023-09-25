6e67ad88f01ef155b71858972cf083e49d20581c
TrungTruc-KingRobert <trucleksk@gmail.com>
2023-09-25 21:40:25 +0700

    5 [Add] Add Right section

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 // components
+import Sidebar from "./components/Sidebar";
 import Analytics from "./components/Analytics";
 import NewUsers from "./components/NewUsers";
 import RecentOrders from "./components/RecentOrders";
-import Sidebar from "./components/Sidebar";
+import RightSection from "./components/RightSection";
+
+// context
+import AppProvider from "./contexts/AppProvider";
 
 const App = () => {
   return (
-    <div className="w-[96%] my-0 mx-auto grid grid-cols-[12rem_auto_23rem] gap-[1.8rem] xl:w-[95%] xl:grid-cols-[7rem_auto_23rem] md:w-full md:grid-cols-[1fr] md:py-0 md:px-[var(--padding-1)]">
-      <Sidebar />
-      <main className="mt-[1.4rem] md:mt-[8rem] md:py-0 md:px-[1rem]">
-        <Analytics />
-        <NewUsers />
-        <RecentOrders />
-      </main>
-    </div>
+    <AppProvider>
+      <div className="w-[96%] my-0 mx-auto grid grid-cols-[12rem_auto_23rem] gap-[1.8rem] xl:w-[95%] xl:grid-cols-[7rem_auto_23rem] md:w-full md:grid-cols-[1fr] md:py-0 md:px-[1.8rem]">
+        <Sidebar />
+        <main className="mt-[1.4rem] md:mt-[8rem] md:py-0 md:px-[1rem]">
+          <Analytics />
+          <NewUsers />
+          <RecentOrders />
+        </main>
+        <RightSection />
+      </div>
+    </AppProvider>
   );
 };
 
--- src/components/NewUsers.tsx
+++ src/components/NewUsers.tsx
@@ -2,14 +2,14 @@ const NewUsers = () => {
   return (
     <section className="mt-[1.3rem]">
       <h2>New Users</h2>
-      <div className="bg-[var(--color-white)] p-[var(--card-padding)] rounded-[var(--card-border-radius)] mt-4 shadow-[0_2rem_3rem_var(--color-light)] flex justify-around flex-wrap gap-[1.4rem] cursor-pointer duration-300 ease delay-0 hover:shadow-none">
+      <div className="bg-[var(--color-white)] p-[var(--card-padding)] rounded-[2rem] mt-4 shadow-[0_2rem_3rem_var(--color-light)] flex justify-around flex-wrap gap-[1.4rem] cursor-pointer duration-300 ease delay-0 hover:shadow-none">
         <div className="flex flex-col items-center justify-center xl:basis-2/5 md:basis-[35%]">
           <img
             src="/profile-2.jpg"
             className="w-20 h-20 mb-[0.4rem] rounded-[50%]"
           />
-          <h2>Jack</h2>
-          <p>54 Min Ago</p>
+          <h2>Jonh</h2>
+          <p>15 Minutes Ago</p>
         </div>
 
         <div className="flex flex-col items-center justify-center xl:basis-2/5 md:basis-[35%]">
@@ -18,7 +18,7 @@ const NewUsers = () => {
             className="w-20 h-20 mb-[0.4rem] rounded-[50%]"
           />
           <h2>Amir</h2>
-          <p>3 Hours Ago</p>
+          <p>5 Hours Ago</p>
         </div>
 
         <div className="flex flex-col items-center justify-center xl:basis-2/5 md:basis-[35%]">
@@ -27,7 +27,7 @@ const NewUsers = () => {
             className="w-20 h-20 mb-[0.4rem] rounded-[50%]"
           />
           <h2>Ember</h2>
-          <p>6 Hours Ago</p>
+          <p>9 Hours Ago</p>
         </div>
 
         <div className="flex flex-col items-center justify-center xl:basis-2/5 md:basis-[35%]">
